397760867680035c639bbb98e006ca0aeb122902
Vyachaslav75 <kslava@tut.by>
2022-12-19 21:02:05 +0300

    Add Task_17

---
+++ Task_17.py
@@ -0,0 +1,54 @@
+import random
+
+
+def write_file(user_list):
+    with open('Index_Position.txt', 'w', encoding='utf-8') as f:
+        if len(user_list):
+            for i in user_list:
+                f.writelines(str(i)+'\n')
+        f.close()
+
+
+def read_file():
+    with open('Index_Position.txt', 'r', encoding='utf-8') as f:
+        result = []
+        for i in f:
+            result.append(int(i))
+        f.close()
+    return result
+
+
+def index_list(count_index):
+    print('Если хотите ввести индексы нажмите 1')
+    print('Для заполнения случайными индексами нажмите 2')
+    user_str = input('Введите номер режима заполнения файла:  ')
+    if user_str == '1':
+        user_str = input(
+            f'Введите номера индексов через запятую, максимум {count_index}:  ')
+        user_str = [int(x) for x in user_str.split(',')]
+    elif user_str == '2':
+        user_str = []
+        for i in range(count_index//2):
+            user_str.append(random.randint(0, count_index))
+    write_file(user_str)
+
+
+while True:
+    try:
+        user_number = int(input('Введите число:  '))
+        break
+    except ValueError:
+        print('Введите целое цисло')
+user_list = []
+for i in range(-user_number, user_number+1):
+    user_list.append(i)
+# print(f'Список для рассчета  {user_list}')
+# write_file(user_list)
+index_list(len(user_list))
+print(f'Список для рассчета  {user_list}')
+index = read_file()
+print(f'Номера индексов для умножения  {index}')
+res = 1
+for i in index:
+    res *= user_list[i]
+print(f'Произведение элементов: {res}')
